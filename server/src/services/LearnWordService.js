2adea0b7ebf2d95bae962f89fae8f24428c3a71c
PlowerEater <127913408+PlowerEater@users.noreply.github.com>
2025-05-23 04:18:15 +0300

    Mergio Conflicto

--- server/src/services/LearnWordService.js
+++ server/src/services/LearnWordService.js
@@ -19,7 +19,6 @@ class LearnWordService {
     return learnWordArr;
   }
 
-  
 
   // Пометить карточку как изученную - создаем запись в таблице об изученном слове
   static async createLearnWordByDb({userId, wordId, themeId}) {
